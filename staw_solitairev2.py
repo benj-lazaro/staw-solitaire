dad601a3a1b55b5f4ac8fac8a3dd224318ba72e5
benj-lazaro <benjie.work@gmail.com>
2023-02-22 23:37:54 +0800

    Refactor comments and parameter names

--- staw_solitairev2.py
+++ staw_solitairev2.py
@@ -2,7 +2,8 @@ from random import shuffle
 
 game_round = 1
 ship_list = []
-is_gameon = True
+is_game_on = True
+
 
 def welcome_screen():
     """welcome_screen() returns a welcome screen."""
@@ -10,7 +11,7 @@ def welcome_screen():
 
 
 def end_screen():
-    """end_screen() returns a thank you message."""
+    """end_screen() returns a thank-you message."""
     print("Thank you for playing Star Trek Attack Wing: Solitaire")
 
 
@@ -20,8 +21,9 @@ def get_ship_name(ship_count, list_data):
         ship_name = str(input("Enter name of ship: "))
         list_data.append(ship_name)
 
+
 def shuffle_ships(list_data):
-    """shuffe_ships(list) returns a randomize list items."""
+    """shuffle_ships(list) returns a randomized list items."""
     return shuffle(list_data)
 
 
@@ -37,25 +39,25 @@ def ship_destroyed(list_data, index_value):
     return list_data.pop(index_value)
 
 
-def movement_phase(list_data, game_round):
-    """movement_phease(list, int) returns a list of ships to move in succession."""
+def movement_phase(list_data, game_rnd):
+    """movement_phase(list, int) returns a list of ships to move in succession."""
     shuffle_ships(list_data)
-    print(f"\nMovement Phase Round {game_round}")
+    print(f"\nMovement Phase Round {game_rnd}")
     return generate_ship_list(list_data, "move")
 
 
-def attack_phase(list_data, game_round):
+def attack_phase(list_data, game_rnd):
     """attack_phase(list, int) returns a list of ship to attack in succession."""
     ship_down = True
     shuffle_ships(list_data)
-    print(f"\nAttack Phase Round {game_round}")
+    print(f"\nAttack Phase Round {game_rnd}")
     generate_ship_list(list_data, "attack")
 
     user_response = str(input("\nAre there any ships destroyed [y/n]? ")).lower()
 
     if user_response == 'y':
         while ship_down:
-            print("Enter the dstroyed ship's index number or x to exit:")
+            print("Enter the destroyed ship's index number or x to exit:")
             ship_index = input("Ship Index Number: ")
 
             if ship_index == "x":
@@ -64,23 +66,24 @@ def attack_phase(list_data, game_round):
                 ship_destroyed(list_data, int(ship_index))
 
 
-def end_phase(game_round):
+def end_phase(game_rnd):
     """end_phase(int) returns multi-line text on end / clean-up phase of the game."""
-    print(f"\nEnd Phase Round {game_round}")
+    print(f"\nEnd Phase Round {game_rnd}")
     print("----")
     print("1.) Flip back disabled Shield token(s) to their active sides for free.")
-    print("2.) Remove all remaining Scan, Evade, Battlestations & red Cloak tokens.")
+    print("2.) Remove all remaining Scan, Evade, Battle stations & red Cloak tokens.")
     print("3.) Ships with green Clock tokens can choose to keep their Shield tokens disabled & remain cloaked.")
     print("4.) Retain all Target Lock tokens.")
     print("----")
 
 
-def declare_victor(ship_list):
+def declare_victor(ship):
     """declare_victor(list) returns the name of the ship that won the game."""
-    return f"\n{ship_list[0]} wins this game!!!\n"
+    return f"\n{ship[0]} wins this game!!!\n"
+
 
 def game_setup():
-    """game_setup() accepts user input, setup the data structure for the game."""
+    """game_setup() accepts user input, set up the data structure for the game."""
     ships_to_play = int(input("How many ships you will play: "))
     ships_to_play_against = int(input("How many ships will you play against: "))
 
@@ -95,28 +98,27 @@ def game_setup():
         get_ship_name(ships_to_play_against, ship_list)
 
 
-
 # Gameplay
 welcome_screen()
 game_setup()
 
-while is_gameon:
+while is_game_on:
     movement_phase(ship_list, game_round)
 
     proceed_to_next_phase = str(input("Proceed to the next phase [y/n]: ")).lower()
     if proceed_to_next_phase == "n":
-        is_gameon = False
+        is_game_on = False
     else:
         attack_phase(ship_list, game_round)
 
         if len(ship_list) == 1:
             print(declare_victor(ship_list))
-            is_gameon = False
+            is_game_on = False
         else:
             end_phase(game_round)
             next_round = str(input("Proceed to the next round [y/n]: ")).lower()
             if next_round == "n":
-                is_gameon = False
+                is_game_on = False
 
             game_round += 1
 
